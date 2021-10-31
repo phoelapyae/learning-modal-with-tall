658e0e3beede594a080baa5859d10935c6355426
zinmyomaung <zinmyomaung.zotefamily@gmail.com>
2021-10-31 22:43:52 +0630

    toggle-alpine-modals-using-events

--- resources/views/components/confirmation-modal.blade.php
+++ resources/views/components/confirmation-modal.blade.php
@@ -1,4 +1,6 @@
-<div{{ $attributes }} x-data="{ show: @entangle($attributes->wire('model'))}" x-show="show" @keydown.escape.window="show = false" style="display: none;">
+@props(['name'])
+
+<div id="{{ $name }}" x-data="{ show: false, name: '{{$name}}'}" x-show="show" x-on:modal.window="show = ($event.detail === name)" @keydown.escape.window="show = false" style="display: none;" {{ $attributes }}>
     <div class="fixed inset-0 bg-gray-900 opacity-90" @click="show = false"></div>
     <div class="bg-white shadow-md p-4 max-w-sm h-48 m-auto rounded-md fixed inset-0" x-whow.transitio="show">
         <div class="flex flex-col h-full justify-between">
@@ -15,4 +17,4 @@
             </footer>
         </div>
     </div>
-    </div>
+</div>
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,3 +1,19 @@
 <x-layout>
-    <livewire:manage-user />
+    <div class="shadow sm:rounded-md sm:overflow-hidden">
+        <div class="px-4 py-5 bg-white space-y-6 sm:p-6">
+            <x-button class="bg-gray-400 hover:bg-gray-500" onclick="$modals.show('join-modal')">Join</x-button>
+        </div>
+    </div>
+
+    <x-modals.join />
+
+    <script>
+        window.$modals = {
+            show(name) {
+                window.dispatchEvent(new CustomEvent('modal', {
+                    detail: name
+                }));
+            }
+        }
+    </script>
 </x-layout>
